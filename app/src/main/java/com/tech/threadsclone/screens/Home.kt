2aeb874913aa2af373166d4303616ff01e7bbe4c
Abhishek Vadodariya <37392803+abhishekvadodariya@users.noreply.github.com>
2023-10-05 18:29:52 +0100

    working on Home screen

--- app/src/main/java/com/tech/threadsclone/screens/Home.kt
+++ app/src/main/java/com/tech/threadsclone/screens/Home.kt
@@ -1,9 +1,32 @@
 package com.tech.threadsclone.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tech.threadsclone.itemView.ThreadItem
+import com.tech.threadsclone.viewModel.HomeViewModel
 
 @Composable
-fun  Home(){
-    Text(text = "Home")
+fun Home() {
+    val context = LocalContext.current
+    val homeViewModel:HomeViewModel = viewModel()
+    val threadAndUser by homeViewModel.threadsAndUsers.observeAsState(null)
+
+    LazyColumn {
+        /*items(threadAndUser?: emptyList<>()) {pairs ->
+
+            ThreadItem()
+        }*/
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowHome() {
+    Home()
 }
---
+++ app/src/main/java/com/tech/threadsclone/viewModel/HomeViewModel.kt
@@ -0,0 +1,71 @@
+package com.tech.threadsclone.viewModel
+
+import android.content.Context
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.tech.threadsclone.model.ThreadModel
+import com.tech.threadsclone.model.UserModel
+import com.tech.threadsclone.utils.SharedPrefrence
+import java.util.UUID
+
+class HomeViewModel : ViewModel() {
+
+    private val fdb: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val thread: DatabaseReference = fdb.getReference("threads")
+
+    private val _threadsAndUsers = MutableLiveData<List<Pair<ThreadModel,UserModel>>>()
+    val threadsAndUsers: LiveData<List<Pair<ThreadModel,UserModel>>> = _threadsAndUsers
+
+    private fun fetchThreadsAndUsers(onResult: (List<Pair<ThreadModel,UserModel>>) -> Unit){
+        thread.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val result = mutableListOf<Pair<ThreadModel,UserModel>>()
+                for (threadsSnapshot in snapshot.children){
+                    val thread = threadsSnapshot.getValue(ThreadModel::class.java)
+                    thread.let {
+                        fetchUserFromThreads(it!!){
+                            user ->
+                            result.add(0,it to user)
+                            if(result.size == snapshot.childrenCount.toInt()){
+                                onResult(result)
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    fun fetchUserFromThreads(thread: ThreadModel, onResult:(UserModel) -> Unit){
+        fdb.getReference("threads").child(thread.userId)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val user = snapshot.getValue(UserModel::class.java)
+                    user?.let { onResult }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+    }
+
+}
